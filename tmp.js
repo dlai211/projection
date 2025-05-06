7d7cc163ccd3c794b2d2555649b9e9ce300157e6
dlai <dlai211@uw.edu>
2025-05-06 11:34:48 -0700

    commit all

---
+++ tmp.js
@@ -0,0 +1,8 @@
+const colors = [
+    { hex: '#CB6040', rgba: 'rgba(203, 96, 64, ' },  // Color 1
+    { hex: '#FD8B51', rgba: 'rgba(253, 139, 81, ' }, // Color 2
+    { hex: '#F2E5BF', rgba: 'rgba(242, 229, 191, ' }, // Color 3
+    { hex: '#257180', rgba: 'rgba(37, 113, 128, ' }  // Color 4
+]; 
+
+console.log
