2d37d48baa7234a1ab1868567926e35e3c5f9bb9
dlai <dlai211@uw.edu>
2025-09-18 12:46:42 -0700

    commit all

--- test.py
+++ test.py
@@ -1,37 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-
-# Define the function
-def function(x, y):
-    return np.cos(x * y)
-
-sample_num = [100, 200, 300, 400] # 100
-
-# Plot heatmap
-plt.figure(figsize=(25, 25))
-
-def Plot(sample_num, i):
-
-    # Create x and y values
-    x = np.linspace(0, 30 * math.pi, sample_num)
-    y = np.linspace(0, 30 * math.pi, sample_num)
-
-    # Create meshgrid for x and y
-    X, Y = np.meshgrid(x, y)
-
-    # Calculate Z values based on the function
-    Z = function(X, Y)
-
-    plt.subplot(221+i)
-    contour = plt.contour(X, Y, Z, 20, cmap='viridis')
-    plt.colorbar(contour, label='cos(x * y)')
-    plt.title(f"Contour Plot of cos(x * y) with {sample_num} samples")
-    plt.xlabel("x")
-    plt.ylabel("y")
-
-
-for i in range(4):
-    Plot(sample_num[i], i)
-
-plt.show()
+# pip install qrcode[pil]
+import qrcode
+u = "https://dlai211.github.io/projection/blackhole_poster/index.html"
+img = qrcode.make(u)
+img.save("qr_universe_on_tap.png")
